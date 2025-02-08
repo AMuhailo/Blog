85cb8a4d15740f03be60f80adcb93b62426060f7
amuhailo25@gmail.com <154220955+AMuhailo@users.noreply.github.com>
2025-02-08 10:18:56 +0200

    Edits allowed hosts

--- myblog/urls.py
+++ myblog/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     path('bosspages/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(), name = 'login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name = 'logout'),
-    path('', DashBoardListView.as_view(), name='dashboard_list_view_url'),
     path('account/',include('account.urls')),
+    path('', DashBoardListView.as_view(), name='dashboard_list_view_url'),
     path('blog/',include('blogs.urls')),
     path('__debug__',include('debug_toolbar.urls')),
 ]
